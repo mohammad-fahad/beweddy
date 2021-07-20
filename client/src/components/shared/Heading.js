865de39e6f2338881e1a7654dbe7065c77517136
MuttakinHasib <hasibmolla28@gmail.com>
2021-07-20 16:26:02 +0600

    :hammer:

--- client/src/components/shared/Heading.js
+++ client/src/components/shared/Heading.js
@@ -1,7 +1,7 @@
 const Heading = ({ label, color }) => {
   return (
     <>
-      <h2 className='text-5xl max-w-5xl mx-auto leading-normal odd text-center mt-16 pb-10'>
+      <h2 className='text-5xl max-w-3xl  mx-auto leading-normal odd text-center pb-10'>
         {label}
       </h2>
       <div
