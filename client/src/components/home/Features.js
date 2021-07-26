31049515c997719c9089cc880288e2e7506322b1
MuttakinHasib <hasibmolla28@gmail.com>
2021-07-27 02:44:34 +0600

    Added Responsive Design for mobile devices :construction: :hammer:

--- client/src/components/home/Features.js
+++ client/src/components/home/Features.js
@@ -7,37 +7,41 @@ const Features = () => {
         label='The Only Free Text Messaging Wedding Platform'
         color='bg-secondary-alternative'
       />
-      <div className='grid md:grid-cols-12 gap-20 max-w-4xl mx-auto'>
-        <div className='col-span-7'>
+      <div className='grid md:grid-cols-12 gap-16 lg:gap-20 max-w-4xl mx-auto'>
+        <div className='col-span-6 md:col-span-7'>
           <div className='space-y-5'>
-            <div className='pr-10 timeline pl-8 py-8 flex items-center space-x-10'>
-              <p className='text-xl font-medium'>
+            <div className='pr-8 lg:pr-10 timeline pl-7 lg:pl-8 py-7 lg:py-8 flex items-center space-x-8 lg:space-x-10'>
+              <p className='text-lg lg:text-xl font-medium'>
                 Text/ Email/ Mail Out Your Invitations & E-Invites
               </p>
-              <img src='/icons/mail.svg' alt='' className='w-14' />
+              <img src='/icons/mail.svg' alt='' className='w-12 lg:w-14' />
             </div>
-            <div className='pl-10 timeline pr-8 py-8 flex items-center space-x-10'>
-              <img src='/icons/louspeaker.svg' alt='' className='w-14' />
-              <p className='text-xl font-medium'>
+            <div className='pl-8 lg:pl-10 timeline pr-7 lg:pr-8 py-7 lg:py-8 flex items-center space-x-8 lg:space-x-10'>
+              <img
+                src='/icons/louspeaker.svg'
+                alt=''
+                className='w-12 lg:w-14'
+              />
+              <p className='text-lg lg:text-xl font-medium'>
                 Send Reminders, Updates & Collect Addresses from the Wedding
                 Party
               </p>
             </div>
-            <div className='pr-10 timeline pl-8 py-8 flex items-center space-x-10'>
-              <p className='text-xl font-medium'>
+            <div className='pr-8 lg:pr-10 timeline pl-7 lg:pl-8 py-7 lg:py-8 flex items-center space-x-8 lg:space-x-10'>
+              <p className='text-lg lg:text-xl font-medium'>
                 Text your Wedding Schedule, RSVP & Itinerary
               </p>
-              <img src='/icons/archive.svg' alt='' className='w-14' />
+              <img src='/icons/archive.svg' alt='' className='w-12 lg:w-14' />
             </div>
-            <div className='pl-10 timeline pr-8 py-8 flex items-center space-x-10'>
-              <img src='/icons/chat.svg' alt='' className='w-14' />
-              <p className='text-xl font-medium'>
+            <div className='pl-8 lg:pl-10 timeline pr-7 lg:pr-8 py-7 lg:py-8 flex items-center space-x-8 lg:space-x-10'>
+              <img src='/icons/chat.svg' alt='' className='w-12 lg:w-14' />
+              <p className='text-lg lg:text-xl font-medium'>
                 Send A Thank You message by Text or E-mail
               </p>
             </div>
           </div>
         </div>
-        <div className='col-span-5'>
+        <div className='col-span-6 md:col-span-5'>
           <img
             src='/images/feature-mobile.png'
             alt=''
--- client/src/components/shared/header/index.js
+++ client/src/components/shared/header/index.js
@@ -5,11 +5,12 @@ import {
   Linkedin,
   Twitter,
 } from '@icons-pack/react-simple-icons';
+import { MenuIcon } from '@heroicons/react/solid';
 
 export const Header = () => {
   return (
     <header className='bg-white'>
-      <div className='container py-10 flex items-center justify-between space-x-16'>
+      <div className='max-w-6xl px-10 xl:px-0 mx-auto xl:container py-10 flex items-center justify-between gap-5 lg:gap-10 xl:gap-16 flex-wrap'>
         {/* Logo */}
         <Link href='/'>
           <a>
@@ -17,10 +18,10 @@ export const Header = () => {
           </a>
         </Link>
         {/* Navigation */}
-        <div className='flex-1 space-y-5'>
+        <div className='flex gap-10 xl:gap-0 xl:block flex-row-reverse flex-1 xl:space-y-5'>
           {/* Navigation Links */}
           <div className='flex items-center justify-between'>
-            <div className='flex items-center space-x-8'>
+            <div className='hidden xl:flex items-center gap-8'>
               <Link href='/'>
                 <a className='flex items-center space-x-3 text-primary font-bold font-inter hover:text-primary transition-colors duration-300'>
                   <img src='/icons/home.svg' alt='' className='w-5 h-5' />
@@ -46,7 +47,7 @@ export const Header = () => {
                 </a>
               </Link>
             </div>
-            <div className='flex items-center space-x-8'>
+            <div className='flex items-center gap-8'>
               <Link href='/login'>
                 <a className='text-primary font-bold font-inter hover:text-primary/60 transition-colors duration-300'>
                   Login
@@ -57,10 +58,14 @@ export const Header = () => {
                   Signup
                 </a>
               </Link>
+
+              <button className='inline-block xl:hidden text-primary font-bold font-inter hover:text-primary/60 transition-colors duration-300'>
+                <MenuIcon className='w-8 h-8' />
+              </button>
             </div>
           </div>
           {/* Searchbar & Social Links */}
-          <div className='flex items-center space-x-20'>
+          <div className='hidden lg:flex flex-1 items-center gap-10 xl:gap-20'>
             <div className='relative flex-1'>
               <input
                 type='text'
@@ -71,7 +76,7 @@ export const Header = () => {
                 <img src='/icons/search.svg' alt='' />
               </button>
             </div>
-            <div className='flex items-center space-x-9'>
+            <div className='hidden xl:flex items-center gap-6 xl:gap-9'>
               <a href='#'>
                 <Facebook
                   size={25}
@@ -97,6 +102,44 @@ export const Header = () => {
                 />
               </a>
             </div>
+          </div>
+        </div>
+        <div className='w-full flex lg:hidden items-center gap-10 xl:gap-20'>
+          <div className='relative flex-1'>
+            <input
+              type='text'
+              className='w-full py-3 px-4 border-2 border-primary border-r-0 rounded-r-none rounded-md text-lg font-inter font-bold placeholder-primary/60 focus:ring-0 focus:border-primary'
+              placeholder='Find a couple'
+            />
+            <button className='absolute -right-2 top-0 bottom-0 border-2 border-primary rounded-md py-3 px-6'>
+              <img src='/icons/search.svg' alt='' />
+            </button>
+          </div>
+          <div className='flex items-center gap-6 xl:gap-9'>
+            <a href='#'>
+              <Facebook
+                size={25}
+                className='text-gray-400 hover:text-[#1877F2] transition-colors duration-300'
+              />
+            </a>
+            <a href='#'>
+              <Twitter
+                size={25}
+                className='text-gray-400 hover:text-[#1DA1F2] transition-colors duration-300'
+              />
+            </a>
+            <a href='#'>
+              <Instagram
+                size={25}
+                className='text-gray-500 hover:text-[#E4405F] transition-colors duration-300'
+              />
+            </a>
+            <a href='#'>
+              <Linkedin
+                size={25}
+                className='text-gray-500 hover:text-[#0A66C2] transition-colors duration-300'
+              />
+            </a>
           </div>
         </div>
       </div>
