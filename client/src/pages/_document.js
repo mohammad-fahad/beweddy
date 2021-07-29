d771eb7abf458bc53b42d13c22920a3a72d8b457
MuttakinHasib <hasibmolla28@gmail.com>
2021-07-29 21:48:18 +0600

    something new :zap: :construction: :hammer:

--- client/src/pages/_document.js
+++ client/src/pages/_document.js
@@ -6,6 +6,7 @@ class MyDocument extends Document {
       <Html lang='en'>
         <Head>
           {/* <meta name='image' content='/images/seo_image.png' /> */}
+
           <meta property='title' content='BeWeddy' />
           <meta name='description' content='' />
           <meta property='og:title' content='' />
@@ -30,6 +31,28 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Alice&family=Inter:wght@100;200;300;400;500;600;700&family=Poppins:wght@700&display=swap'
             rel='stylesheet'
           />
+
+          <link
+            rel='apple-touch-icon'
+            sizes='180x180'
+            href='/apple-touch-icon.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+            href='/favicon-32x32.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+            href='/favicon-16x16.png'
+          />
+          <link rel='manifest' href='/site.webmanifest' />
+          <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+          <meta name='msapplication-TileColor' content='#da532c' />
+          <meta name='theme-color' content='#ffffff' />
         </Head>
         <body>
           <Main />
