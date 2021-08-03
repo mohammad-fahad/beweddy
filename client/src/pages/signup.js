b6ba1904a67e00965d54b17d9a1aa6c37222ce00
MuttakinHasib <hasibmolla28@gmail.com>
2021-08-03 19:30:56 +0600

    Responsive :hammer_and_wrench:

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -34,7 +34,7 @@ const SignupPage = () => {
         <div className='container min-h-screen flex items-center justify-center'>
           <form className='w-full' onSubmit={handleSubmit(onSubmit)}>
             <section
-              className={`bg-white border-4 my-10 border-primary py-8 md:py-10 px-10 md:px-24 max-w-xl w-full mx-auto rounded-xl`}
+              className={`bg-white border-4 my-5 md:my-10 border-primary py-8 md:py-10 px-10 md:px-24 max-w-xl w-full mx-auto rounded-xl`}
             >
               <div className='text-center mb-3'>
                 <Link href='/'>
@@ -53,7 +53,7 @@ const SignupPage = () => {
                 className='!pb-5 md:!text-[2.55rem]'
                 lineStyle={{ marginBottom: '30px' }}
               />
-              <div className='flex flex-col items-center justify-center space-y-5'>
+              <div className='flex flex-col items-center justify-center space-y-3 md:space-y-5'>
                 <div className='flex items-center gap-3 sm:space-x-4 justify-center flex-wrap mb-3'>
                   <div className='flex items-center'>
                     <input
@@ -68,10 +68,10 @@ const SignupPage = () => {
                       htmlFor='couple'
                       className='flex items-center space-x-3 cursor-pointer'
                     >
-                      <div className='checked-outer border-[3px] rounded-full border-primary w-7 h-7 flex items-center justify-center'>
-                        <div className='checked-inner w-3 h-3 rounded-full'></div>
+                      <div className='checked-outer border-[3px] rounded-full border-primary w-6 md:w-7 h-6 md:h-7 flex items-center justify-center'>
+                        <div className='checked-inner w-2 md:w-3 h-2 md:h-3 rounded-full'></div>
                       </div>
-                      <span className='font-inter text-lg font-bold'>
+                      <span className='font-inter text-sm md:text-lg font-bold'>
                         For Couple
                       </span>
                     </label>
@@ -88,23 +88,23 @@ const SignupPage = () => {
                       htmlFor='venue'
                       className='flex items-center space-x-3 cursor-pointer'
                     >
-                      <div className='checked-outer border-[3px] rounded-full border-primary w-7 h-7 flex items-center justify-center'>
-                        <div className='checked-inner w-3 h-3 rounded-full'></div>
+                      <div className='checked-outer border-[3px] rounded-full border-primary w-6 md:w-7 h-6 md:h-7 flex items-center justify-center'>
+                        <div className='checked-inner w-2 md:w-3 h-2 md:h-3 rounded-full'></div>
                       </div>
-                      <span className='font-inter text-lg font-bold'>
+                      <span className='font-inter text-sm md:text-lg font-bold'>
                         For Venue
                       </span>
                     </label>
                   </div>
                 </div>
                 <button className='!mb-3 border-2 text-sm md:text-base border-primary py-3 px-4 md:px-12 flex items-center space-x-3 rounded-[100px]'>
-                  <img src='/icons/gmail.svg' alt='' className='w-5 h-5' />
-                  <span>Start with Google</span>
+                  <img src='/icons/gmail.svg' alt='' className='w-6 h-5' />
+                  <spa6>Start with Google</spa6>
                 </button>
                 <div className='w-full'>
                   <input
                     type='email'
-                    className='w-full text-lg font-normal py-3 px-4 placeholder-gray-400 border-[3px] border-primary rounded-[10px]'
+                    className='w-full text-sm md:text-lg font-normal py-2 md:py-3 px-4 placeholder-gray-400 border-[3px] border-primary rounded-[10px]'
                     placeholder='Your Email'
                     {...register('email', {
                       required: {
@@ -127,7 +127,7 @@ const SignupPage = () => {
                 <div className='w-full'>
                   <input
                     type='password'
-                    className='w-full text-lg font-normal py-3 px-4 placeholder-gray-400 border-[3px] border-primary rounded-[10px]'
+                    className='w-full text-sm md:text-lg font-normal py-2 md:py-3 px-4 placeholder-gray-400 border-[3px] border-primary rounded-[10px]'
                     placeholder='Password'
                     {...register('password', {
                       required: {
@@ -149,7 +149,7 @@ const SignupPage = () => {
                 <div className='w-full'>
                   <button
                     type='submit'
-                    className='w-full py-3 px-4 placeholder-gray-400 border-2 border-primary bg-primary hover:bg-primary/80 text-white rounded-lg flex items-center justify-center space-x-3 transition duration-300'
+                    className='w-full py-2 md:py-3 px-4 text-sm md:text-base placeholder-gray-400 border-2 border-primary bg-primary hover:bg-primary/80 text-white rounded-lg flex items-center justify-center space-x-3 transition duration-300'
                   >
                     <img src='/icons/signup.svg' alt='' className='w-6 h-6' />
                     <span>Signup</span>
@@ -167,7 +167,7 @@ const SignupPage = () => {
                 </div>
                 <div className='max-w-xs w-full mx-auto h-[2px] bg-gray-200'></div> */}
                 <div className='w-full'>
-                  <p className='font-light'>
+                  <p className='font-light text-sm md:text-base'>
                     By creating an account you accept our{' '}
                     <Link href='/'>
                       <a className='font-inter font-semibold hover:underline'>
