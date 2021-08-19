638a453119d0b383ca976bdd7c4b79848c52ae85
MuttakinHasib <hasibmolla28@gmail.com>
2021-08-19 23:39:39 +0600

    updated rsvp preview page :zap: :construction: :hammer_and_wrench:

--- client/src/components/home/WelcomeAlert.js
+++ client/src/components/home/WelcomeAlert.js
@@ -8,7 +8,7 @@ const WelcomeAlert = () => {
       <div className='max-w-[1350px] px-10 xxl:px-0 mx-auto flex items-center justify-between space-x-10'>
         <p className='text-sm text-medium'>Eat, Drink & BeWeddy!</p>
         <button className='px-5 py-2' onClick={() => setClose(true)}>
-          <XIcon className='w-6 h-6' />
+          <XIcon className='w-6 h-6 text-gray-400 hover:text-primary transition' />
         </button>
       </div>
     </div>
