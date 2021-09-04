2e9651cb8e4d718fd793be0f0311e9c3786b17f8
Hasib Molla <hasibmolla28@gmail.com>
2021-09-04 20:21:49 +0600

    Sonjoy (#72)
    * do website page one
    
    * 50% done dashboard website page
    
    * deashboard website page 1 and website page 2 is done with full functionality
    
    Co-authored-by: sonjoybarmon <sonjoybarmon19@gmail.com>

---
+++ client/src/components/dashboard/Website/WebsiteNav.jsx
@@ -0,0 +1,50 @@
+import { logout } from "@features/auth/authSlice";
+import { Menu, Transition } from "@headlessui/react";
+import { LogoutIcon, UserIcon, SearchIcon } from "@heroicons/react/outline";
+import Link from "next/link";
+import { useDispatch, useSelector } from "react-redux";
+
+const WebsiteNav = ({ coupleName }) => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
+  console.log(user);
+  return (
+    <div className="bg-[#ffffff] border-b-[3px] border-primary">
+      <div className=" xxl:pr-0 ml-14">
+        <div className="flex items-center justify-between">
+          <h3 className="flex text-lg capitalize">
+            <img src="/apple-touch-icon.png" alt="help" className="w-7 h-7" />
+            <span className="pl-2 text-2xl font-medium">
+              {" "}
+              {user?.coupleName}
+            </span>
+          </h3>
+          <div className="flex items-center py-5 space-x-5 pr-7">
+            <Link href="/">
+              <a className="flex items-center space-x-3 text-base font-semibold text-gray-700 transition duration-300 font-inter hover:text-primary">
+                <span>Need yout Address- RSVP</span>
+              </a>
+            </Link>
+            <Link href="/">
+              <a className="flex items-center space-x-3 text-base font-semibold text-gray-700 transition duration-300 font-inter hover:text-primary">
+                <span>Back to Dashboard</span>
+              </a>
+            </Link>
+            <Link href="/">
+              <a className="flex items-center space-x-3 text-base font-semibold text-gray-700 transition duration-300 font-inter hover:text-primary">
+                <span>Contact</span>
+              </a>
+            </Link>
+            <Link href="/">
+              <a className="flex items-center space-x-3 text-base font-semibold text-gray-700 transition duration-300 font-inter hover:text-primary">
+                <span>Follow us</span>
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WebsiteNav;
