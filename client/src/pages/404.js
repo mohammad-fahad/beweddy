b8ded7650841d00d6acdcb573bba8415610a46da
Asraful Islam <asraful@iMac.local>
2021-09-04 22:24:22 +0600

    Refactor UI :zap: :hammer:

--- client/src/pages/404.js
+++ client/src/pages/404.js
@@ -2,18 +2,20 @@ import Link from 'next/link';
 
 const NotFoundPage = () => {
   return (
-    <div className='container py-16 min-h-screen flex flex-col space-y-8 items-center justify-center'>
-      <div className='max-w-lg mx-auto'>
-        <img src='/images/404.svg' alt='' className='w-80' />
+    <div className='gradient'>
+      <div className='container py-16 min-h-screen flex flex-col space-y-8 items-center justify-center'>
+        <div className='max-w-lg mx-auto'>
+          <img src='/images/404.svg' alt='' className='w-80' />
+        </div>
+        <h2 className='text-lg md:text-3xl text-center'>
+          The page you've requested is not available
+        </h2>
+        <Link href='/'>
+          <a className='font-inter py-2 sm:py-3 px-5 sm:px-8 text-white sm:text-sm bg-primary font-medium rounded hover:bg-transparent border-[3px] border-primary hover:text-black'>
+            Back to home
+          </a>
+        </Link>
       </div>
-      <h2 className='text-lg md:text-3xl text-center'>
-        The page you've requested is not available
-      </h2>
-      <Link href='/'>
-        <a className='font-inter py-2 sm:py-3 px-5 sm:px-8 text-white sm:text-sm bg-primary font-medium rounded hover:bg-transparent border-[3px] border-primary hover:text-black'>
-          Back to home
-        </a>
-      </Link>
     </div>
   );
 };
