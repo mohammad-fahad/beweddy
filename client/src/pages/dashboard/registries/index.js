05ff64fcaab429cd2c99bc40c5d8344a44f6f76c
MuttakinHasib <hasibmolla28@gmail.com>
2021-09-09 23:16:20 +0600

    Refactor Code :zap: :construction: :hammer_and_wrench:

--- client/src/pages/dashboard/registries/index.js
+++ client/src/pages/dashboard/registries/index.js
@@ -3,50 +3,18 @@ import Link from 'next/link';
 import { DashboardHeader } from '@components/dashboard';
 import DashboardTopBar from '@components/dashboard/header/TopBar';
 import DashboardLayout from '@components/dashboard/layout';
-import { Button, Footer } from '@components/index';
+import { Button, Footer, Loader } from '@components/index';
 import { LinkIcon, PencilIcon } from '@heroicons/react/outline';
 import { Fragment } from 'react';
 import { withAuthRoute } from '@hoc/withAuthRoute';
 import Image from 'next/image';
 import DashboardContainer from '@components/dashboard/DashboardContainer';
-
-const registries = [
-  {
-    title: 'Amazon',
-    link: 'https://github.com/muttakinhasib',
-    image: '/images/registries/Amazon.png',
-  },
-  {
-    id: 2,
-    title: 'Bed Bath & Beyond',
-    buttonText: 'Link',
-    link: 'https://github.com/muttakinhasib',
-    image: '/images/registries/bbbLogo.png',
-  },
-  {
-    id: 2,
-    title: 'Bed Bath & Beyond',
-    buttonText: 'Link',
-    link: 'https://github.com/muttakinhasib',
-    image: '/images/registries/bbbLogo.png',
-  },
-  {
-    id: 2,
-    title: 'Bed Bath & Beyond',
-    buttonText: 'Link',
-    link: 'https://github.com/muttakinhasib',
-    image: '/images/registries/bbbLogo.png',
-  },
-  {
-    id: 2,
-    title: 'Bed Bath & Beyond',
-    buttonText: 'Link',
-    link: 'https://github.com/muttakinhasib',
-    image: '/images/registries/bbbLogo.png',
-  },
-];
+import { useQuery } from 'react-query';
+import { getRegistries } from '@services/Registry';
 
 const RegistriesPage = () => {
+  const { data: registries, isLoading } = useQuery('registries', getRegistries);
+  if (isLoading) return <Loader />;
   return (
     <Fragment>
       <Head>
@@ -59,45 +27,49 @@ const RegistriesPage = () => {
         <DashboardContainer>
           <div className='space-y-20'>
             <div class='grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6 registry-grid'>
-              {registries?.map(registry => (
-                <div
-                  key={registry.id}
-                  className='p-4 max-w-[250px] bg-white border-2 rounded-md border-gray-200 hover:border-primary transition duration-300 w-full mx-auto'
-                >
-                  <div
-                    className={`border-2 border-primary flex items-center justify-center h-[140px] w-full rounded-[10px] transition duration-300 relative group p-3`}
-                  >
-                    <Link href={registry.link}>
-                      <a
-                        target='_blank'
-                        className='max-w-[273px] rounded-lg flex items-center justify-center w-full h-full opacity-0 hover:opacity-100 absolute inset-0 z-50 group-hover:bg-black/50'
+              {registries?.length <= 0
+                ? 'No registries found'
+                : registries?.map(registry => (
+                    <div
+                      key={registry._id}
+                      className='p-4 max-w-[250px] bg-white border-2 rounded-md border-gray-200 hover:border-primary transition duration-300 w-full mx-auto'
+                    >
+                      <div
+                        className={`border-2 border-primary flex items-center justify-center h-[140px] w-full rounded-[10px] transition duration-300 relative group p-3`}
                       >
-                        <LinkIcon className='w-8 h-8 text-white' />
-                      </a>
-                    </Link>
-                    <div>
-                      <Image
-                        width={200}
-                        height={80}
-                        src={registry.image || '/images/registries/Amazon.png'}
-                      />
-                    </div>
-                  </div>
+                        <Link href={registry.link}>
+                          <a
+                            target='_blank'
+                            className='max-w-[273px] rounded-lg flex items-center justify-center w-full h-full opacity-0 hover:opacity-100 absolute inset-0 z-50 group-hover:bg-black/50'
+                          >
+                            <LinkIcon className='w-8 h-8 text-white' />
+                          </a>
+                        </Link>
+                        <div>
+                          <Image
+                            width={200}
+                            height={80}
+                            src={
+                              registry.image || '/images/registries/Amazon.png'
+                            }
+                          />
+                        </div>
+                      </div>
 
-                  <div className='py-4 text-center flex flex-col'>
-                    <h3 className='text-lg font-semibold font-inter'>
-                      {registry.title}
-                    </h3>
-                    <div>
-                      <button className='py-2 inline-block px-8 border-gray-900 border-2 rounded-[5px] mt-5 hover:bg-black transition duration-300 hover:text-white font-inter font-bold'>
-                        Link
-                      </button>
+                      <div className='py-4 text-center flex flex-col'>
+                        <h3 className='text-lg font-semibold font-inter'>
+                          {registry.title}
+                        </h3>
+                        <div>
+                          <button className='py-2 inline-block px-8 border-gray-900 border-2 rounded-[5px] mt-5 hover:bg-black transition duration-300 hover:text-white font-inter font-bold'>
+                            Link
+                          </button>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-              ))}
+                  ))}
             </div>
-            <div className='flex items-center space-x-5 flex-wrap sm:flex-nowrap'>
+            {/* <div className='flex items-center space-x-5 flex-wrap sm:flex-nowrap'>
               <Button
                 label='Back'
                 className='opacity-50 !rounded-[5px] !py-2 mx-0'
@@ -108,7 +80,7 @@ const RegistriesPage = () => {
                 type='submit'
                 className='mx-0 !py-2 !rounded-[5px]'
               />
-            </div>
+            </div> */}
           </div>
         </DashboardContainer>
       </DashboardLayout>
--- server/controllers/registryController.js
+++ server/controllers/registryController.js
@@ -3,32 +3,32 @@ import Registry from '../models/Registry.js';
 
 // Get All Registry
 export const getRegistry = asyncHandler(async (req, res) => {
-    const registries = await Registry.find({});
-    res.status(200).json({ registries });
+  const registries = await Registry.find({});
+  res.status(200).json(registries);
 });
 
 // Create New Registry
 export const createRegistry = asyncHandler(async (req, res) => {
-    const { title, description, link, image } = req.body;
-    const registry = await Registry.create({ title, description, link, image });
+  const { title, description, link, image } = req.body;
+  const registry = await Registry.create({ title, description, link, image });
 
-    if (registry) {
-        res.status(201).json({ message: 'Registry created successfully' });
-    } else {
-        res.status(500);
-        throw new Error('Server Error');
-    }
+  if (registry) {
+    res.status(201).json({ message: 'Registry created successfully' });
+  } else {
+    res.status(500);
+    throw new Error('Server Error');
+  }
 });
 
 // Delete Registry by ID
 export const deleteRegistry = asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    const registry = Registry.findByIdAndRemove(id);
+  const { id } = req.params;
+  const registry = Registry.findByIdAndRemove(id);
 
-    // if (Registry) {
-    //     res.status(201).json({ message: 'Registry Deleted successfully' });
-    // } else {
-    //     res.status(500);
-    //     throw new Error('Server Error');
-    // }
+  // if (Registry) {
+  //     res.status(201).json({ message: 'Registry Deleted successfully' });
+  // } else {
+  //     res.status(500);
+  //     throw new Error('Server Error');
+  // }
 });
