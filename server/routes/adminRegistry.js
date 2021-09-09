e2d8ec0e14f87c1a04a8942abf4aba22e7b5c2e2
Azim Uddin Ahamed <azim@sqbear.com.au>
2021-09-09 14:47:42 +0600

    changes and deleted registry section

--- server/routes/adminRegistry.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createRegistry } from '../controllers/adminRegistry.js';
-
-const router = express.Router();
-
-router.post('/create', createRegistry);
-
-export default router;
