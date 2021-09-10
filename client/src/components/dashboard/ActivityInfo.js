8c6671b8ed439caaead0da282ea4dfd67bddab0d
sonjoybarmon <sonjoybarmon19@gmail.com>
2021-09-10 08:34:02 +0600

    home change modify is done

--- client/src/components/dashboard/ActivityInfo.js
+++ client/src/components/dashboard/ActivityInfo.js
@@ -1,65 +1,63 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 const ActivityInfo = () => {
   return (
-    <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-3 xl:gap-6'>
-      <div className='p-5 flex flex-col justify-center items-center space-y-2 transition duration-300 text-center'>
+    <div className="grid grid-cols-2 gap-3 sm:grid-cols-3 md:grid-cols-5 xl:gap-6">
+      <div className="flex flex-col items-center justify-center p-5 space-y-2 text-center transition duration-300">
         <Image
-          src='/icons/message-notif.svg'
-          alt='Text Invitation'
+          src="/icons/message-notif.svg"
+          alt="Text Invitation"
           height={38}
           width={38}
         />
-        <h4 className='text-2xl font-medium'>280</h4>
-        <p className='text-sm font-medium whitespace-nowrap'>
-          Text Invitations
-        </p>
+        <h4 className="text-2xl font-medium">280</h4>
+        <p className="text-sm font-medium whitespace-nowrap">Text Invitation</p>
       </div>
-      <div className='p-5 flex flex-col justify-center items-center space-y-2 transition duration-300 text-center'>
+      <div className="flex flex-col items-center justify-center p-5 space-y-2 text-center transition duration-300">
         <Image
-          src='/icons/sms-edit.svg'
-          alt='Email Invitations'
+          src="/icons/sms-edit.svg"
+          alt="Email Invitations"
           height={38}
           width={38}
         />
-        <h4 className='text-2xl font-medium'>390</h4>
-        <p className='text-sm font-medium whitespace-nowrap'>
+        <h4 className="text-2xl font-medium">390</h4>
+        <p className="text-sm font-medium whitespace-nowrap">
           Email Invitations
         </p>
       </div>
-      <div className='p-5 flex flex-col justify-center items-center space-y-2 transition duration-300 text-center'>
+      <div className="flex flex-col items-center justify-center p-5 space-y-2 text-center transition duration-300">
         <Image
-          src='/icons/sms-tracking.svg'
-          alt='Mail Out Invitations'
+          src="/icons/sms-tracking.svg"
+          alt="Mail Out Invitations"
           height={38}
           width={38}
         />
-        <h4 className='text-2xl font-medium'>400</h4>
-        <p className='text-sm font-medium whitespace-nowrap'>
-          Mail Out Invitations
+        <h4 className="text-2xl font-medium">400</h4>
+        <p className="text-sm font-medium whitespace-nowrap">
+          Mail Out Invitation
         </p>
       </div>
-      <div className='p-5 flex flex-col justify-center items-center space-y-2 transition duration-300 text-center'>
+      <div className="flex flex-col items-center justify-center p-5 space-y-2 text-center transition duration-300">
         <Image
-          src='/icons/location-tick.svg'
-          alt='Addresses Collected'
+          src="/icons/location-tick.svg"
+          alt="Addresses Collected"
           height={38}
           width={38}
         />
-        <h4 className='text-2xl font-medium'>130</h4>
-        <p className='text-sm font-medium whitespace-nowrap'>
+        <h4 className="text-2xl font-medium">130</h4>
+        <p className="text-sm font-medium whitespace-nowrap">
           Addresses Collected
         </p>
       </div>
-      <div className='p-5 flex flex-col justify-center items-center space-y-2 transition duration-300 text-center'>
+      <div className="flex flex-col items-center justify-center p-5 space-y-2 text-center transition duration-300">
         <Image
-          src='/icons/directbox-notif.svg'
-          alt='RSVP'
+          src="/icons/directbox-notif.svg"
+          alt="RSVP"
           height={38}
           width={38}
         />
-        <h4 className='text-2xl font-medium'>138</h4>
-        <p className='text-sm font-medium whitespace-nowrap'>RSVP</p>
+        <h4 className="text-2xl font-medium">138</h4>
+        <p className="text-sm font-medium whitespace-nowrap">RSVP</p>
       </div>
     </div>
   );
