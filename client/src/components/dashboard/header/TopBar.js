d41af6a1f2bce7add0a40155afb48be9fde76f66
sonjoybarmon <sonjoybarmon19@gmail.com>
2021-09-11 17:51:40 +0600

    work on dashboard

--- client/src/components/dashboard/header/TopBar.js
+++ client/src/components/dashboard/header/TopBar.js
@@ -1,71 +1,73 @@
-import { logout } from '@features/auth/authSlice';
-import { Menu, Transition } from '@headlessui/react';
-import { LogoutIcon, UserIcon, SearchIcon } from '@heroicons/react/outline';
-import Link from 'next/link';
-import { useDispatch, useSelector } from 'react-redux';
+import { logout } from "@features/auth/authSlice";
+import { Menu, Transition } from "@headlessui/react";
+import { LogoutIcon, UserIcon, SearchIcon } from "@heroicons/react/outline";
+import Link from "next/link";
+import { useDispatch, useSelector } from "react-redux";
 
 const DashboardTopBar = () => {
   const dispatch = useDispatch();
-  const { user } = useSelector(state => state.user);
+  const { user } = useSelector((state) => state.user);
   return (
-    <div className='bg-secondary-alternative border-b-[3px] border-primary'>
-      <div className='max-w-[1620px] pr-12 md:pr-16 xxl:pr-0 ml-6 sm:ml-14'>
-        <div className='flex md:items-center justify-between py-5 flex-col sm:flex-row space-y-3 sm:space-y-0'>
-          <h3 className='text-xl capitalize'>👋 Welcome {user?.coupleName}!</h3>
-          <div className='flex items-center space-x-5'>
-            {/* <Link href='/'>
-              <a className='flex items-center space-x-2 md:space-x-3 font-inter text-sm md:text-base text-gray-700 hover:text-primary font-semibold transition duration-300'>
-                <SearchIcon className='w-5 h-5 sm:w-6 sm:h-6' />
-                <span>Search</span>
-              </a>
-            </Link> */}
-            <Link href='#'>
-              <a className='flex items-center space-x-2 md:space-x-3 font-inter text-sm md:text-base text-gray-700 hover:text-primary font-semibold transition duration-300'>
-                <img
-                  src='/icons/lifebuoy.svg'
-                  alt='help'
-                  className='w-5 h-5 sm:w-6 sm:h-6'
-                />
-                <span>Help</span>
+    <div className="bg-secondary-alternative border-b-[3px] border-primary">
+      <div className="max-w-[1620px] pr-12 md:pr-16 xxl:pr-0 ml-6 sm:ml-14">
+        <div className="flex flex-col justify-between py-5 space-y-3 md:items-center sm:flex-row sm:space-y-0">
+          <h3 className="text-base capitalize">
+            👋 Welcome {user?.coupleName}!
+          </h3>
+          <div className="flex items-center space-x-5">
+            <Link href="/">
+              <a className="flex items-center space-x-2 text-sm font-semibold text-gray-700 transition duration-300 md:space-x-3 font-inter md:text-base hover:text-primary">
+                <SearchIcon className="w-[14px] h-[14px]" />
+                <span className="text-[14px]">Search</span>
               </a>
             </Link>
-            <Menu as='div' className='relative'>
+            <Link href="#">
+              <a className="flex items-center space-x-2 text-sm font-semibold text-gray-700 transition duration-300 md:space-x-3 font-inter md:text-base hover:text-primary">
+                <img
+                  src="/icons/lifebuoy.svg"
+                  alt="help"
+                  className="w-[14px] h-[14px]"
+                />
+                <span className="text-[14px]">Help</span>
+              </a>
+            </Link>
+            <Menu as="div" className="relative">
               {({ open }) => (
                 <>
-                  <Menu.Button className='flex items-center space-x-2 md:space-x-3 font-inter text-sm md:text-base text-gray-700 hover:text-primary font-semibold transition duration-300'>
+                  <Menu.Button className="flex items-center space-x-2 text-sm font-semibold text-gray-700 transition duration-300 md:space-x-3 font-inter md:text-base hover:text-primary">
                     <img
-                      src='/icons/profile-2user.svg'
-                      alt='account'
-                      className='w-5 h-5 sm:w-6 sm:h-6'
+                      src="/icons/profile-2user.svg"
+                      alt="account"
+                      className="w-[14px] h-[14px] "
                     />
-                    <span>Account</span>
+                    <span className="text-[14px]">Account</span>
                   </Menu.Button>
                   <Transition
                     show={open}
-                    as='div'
-                    enter='transition ease-out duration-100'
-                    enterFrom='transform opacity-0 scale-95'
-                    enterTo='transform opacity-100 scale-100'
-                    leave='transition ease-in duration-75'
-                    leaveFrom='transform opacity-100 scale-100'
-                    leaveTo='transform opacity-0 scale-95'
+                    as="div"
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
                   >
                     <Menu.Items
                       // static
-                      // className='origin-top-right z-50 absolute right-0 mt-2 w-52 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none'
-                      className='absolute z-50 right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'
+                      // className='absolute right-0 z-50 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg w-52 ring-1 ring-black ring-opacity-5 focus:outline-none'
+                      className="absolute right-0 z-50 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                     >
                       <Menu.Item>
                         {({ active }) => (
-                          <Link href='/'>
+                          <Link href="/">
                             <a
                               className={`group font-inter ${
-                                active ? 'bg-gray-100' : 'hover:bg-gray-100'
+                                active ? "bg-gray-100" : "hover:bg-gray-100"
                               } hover:bg-gray-100 text-gray-600 flex items-center w-full px-3 py-2 text-base transition duration-300`}
                             >
                               <UserIcon
-                                className='w-5 h-5 mr-2'
-                                aria-hidden='true'
+                                className="w-5 h-5 mr-2"
+                                aria-hidden="true"
                               />
                               Home
                             </a>
@@ -76,13 +78,13 @@ const DashboardTopBar = () => {
                         {({ active }) => (
                           <button
                             className={`group font-inter ${
-                              active ? 'bg-gray-100' : 'hover:bg-gray-100'
+                              active ? "bg-gray-100" : "hover:bg-gray-100"
                             } text-gray-600 flex items-center w-full px-3 py-2 text-base transition duration-300`}
                             onClick={() => dispatch(logout())}
                           >
                             <LogoutIcon
-                              className='w-5 h-5 mr-2'
-                              aria-hidden='true'
+                              className="w-5 h-5 mr-2"
+                              aria-hidden="true"
                             />
                             Log out
                           </button>
@@ -93,14 +95,14 @@ const DashboardTopBar = () => {
                 </>
               )}
             </Menu>
-            <Link href='#'>
-              <a className='!ml-auto sm:!ml-5 relative font-inter text-sm md:text-base text-gray-700 hover:text-primary font-semibold transition duration-300'>
+            <Link href="#">
+              <a className="!ml-auto sm:!ml-5 relative font-inter text-sm md:text-base text-gray-700 hover:text-primary font-semibold transition duration-300">
                 <img
-                  src='/icons/notification.svg'
-                  alt='notification'
-                  className='w-5 h-5 sm:w-6 sm:h-6'
+                  src="/icons/notification.svg"
+                  alt="notification"
+                  className="w-[14px] h-[14px] "
                 />
-                <span className='absolute top-[-13px] right-[-25px] w-[25px] h-[25px] text-[7px] flex items-center justify-center bg-[#FFB1B6] rounded-full'>
+                <span className="absolute top-[-13px] right-[-25px] w-[25px] h-[25px] text-[7px] flex items-center justify-center bg-[#FFB1B6] rounded-full">
                   0
                 </span>
               </a>
--- client/src/components/dashboard/layout.js
+++ client/src/components/dashboard/layout.js
@@ -1,38 +1,38 @@
-import Link from 'next/link';
-import Drawer from './drawer';
-import Sidebar from './Sidebar';
+import Link from "next/link";
+import Drawer from "./drawer";
+import Sidebar from "./Sidebar";
 
 const DashboardLayout = ({ children, shadow }) => {
   return (
-    <div className='flex lg:space-x-10 xl:space-x-16 pb-12 flex-wrap lg:flex-nowrap'>
-      <div className='lg:max-w-xs w-full'>
-        {/* <div className='grid grid-cols-2 lg:grid-cols-none px-12 lg:px-0'> */}
-        <div className='flex flex-row lg:flex-col items-center lg:items-start gap-x-5 gap-y-1 lg:gap-0 justify-between flex-wrap px-6 sm:px-12 lg:px-0'>
+    <div className="flex flex-wrap pb-12 lg:space-x-10 xl:space-x-16 lg:flex-nowrap">
+      <div className="w-full lg:max-w-xs">
+        {/* <div className='grid grid-cols-2 px-12 lg:grid-cols-none lg:px-0'> */}
+        <div className="flex flex-row flex-wrap items-center justify-between px-6 lg:flex-col lg:items-start gap-x-5 gap-y-1 lg:gap-0 sm:px-12 lg:px-0">
           <div
             className={`lg:pl-14 lg:min-h-[160px] flex lg:items-center py-5 lg:py-0`}
           >
-            <Link href='/dashboard'>
+            <Link href="/dashboard">
               <a className={`inline-block space-y-2`}>
                 <img
-                  src='/images/logo.png'
-                  alt=''
-                  className='h-14 md:h-[4.5rem]'
+                  src="/images/logo.png"
+                  alt=""
+                  className="h-[88px]"
                 />
-                <h3 className='text-base md:text-lg font-medium'>
+                <h3 className="text-base font-medium ">
                   All-In-One Wedding Platform.
                 </h3>
               </a>
             </Link>
           </div>
-          {/* <div className='col-span-1 self-center lg:col-span-full'> */}
+          {/* <div className='self-center col-span-1 lg:col-span-full'> */}
           <Sidebar />
           {/* </div> */}
         </div>
       </div>
       {shadow ? (
-        <div className='w-full'>{children}</div>
+        <div className="w-full">{children}</div>
       ) : (
-        <div className='w-full container pr-16 xxl:pr-0'>{children}</div>
+        <div className="container w-full pr-16 xxl:pr-0">{children}</div>
       )}
     </div>
   );
