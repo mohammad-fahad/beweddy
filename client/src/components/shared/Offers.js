6a66c2c355dce891cdf24906c2ed7d097b3c9de1
Rayhan Islam <67514865+Rayhan0Islam0Shagor@users.noreply.github.com>
2021-09-12 15:55:05 +0600

    Collect Address & RSVP (#101)
    * new page update
    
    * font-size and some fix
    
    * update
    
    * update responsive

--- client/src/components/shared/Offers.js
+++ client/src/components/shared/Offers.js
@@ -3,23 +3,24 @@ const { Heading } = require('@components/shared');
 const Offers = ({ offers, className, title }) => {
   return (
     <div className={`border-t-4 border-primary ${className ? className : ''}`}>
-      <div className='container py-20'>
+      <div className="container py-20">
         <Heading
           label={title}
-          color='bg-[#F9D1DE]'
-          className='!max-w-4xl drop-shadow-2xl'
+          color="bg-[#F9D1DE]"
+          className="!max-w-4xl drop-shadow-2xl"
           // lineStyle={{ marginBottom: '45px' }}
         />
-        <div className='grid md:grid-cols-2 gap-20'>
+
+        <div className="grid gap-20 md:grid-cols-2">
           {offers.map((offer, index) => (
-            <div key={index} className='max-w-md w-full mx-auto'>
-              <div className='mb-3'>
-                <img src={offer.icon} alt='' className='w-12 lg:w-14' />
+            <div key={index} className="w-full max-w-md mx-auto">
+              <div className="mb-3">
+                <img src={offer.icon} alt="" className="w-12 lg:w-14" />
               </div>
-              <h1 className='mb-3 text-3xl lg:text-4xl font-normal !leading-[146%]'>
+              <h1 className="mb-3 text-3xl lg:text-4xl font-normal !leading-[146%]">
                 {offer.title}
               </h1>
-              <p className='text-base lg:text-lg font-regular text-gray-700'>
+              <p className="text-base text-gray-700 lg:text-lg font-regular">
                 {offer.paragraph}
               </p>
             </div>
