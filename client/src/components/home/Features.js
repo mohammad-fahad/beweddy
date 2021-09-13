bf2a401bbc573d50604522dcd9989348252666bd
Hasib Molla <hasibmolla28@gmail.com>
2021-09-13 18:59:16 +0600

    :zap: :construction: :hammer_and_wrench: (#116)

--- client/src/components/home/Features.js
+++ client/src/components/home/Features.js
@@ -10,14 +10,14 @@ const Features = () => {
           color="bg-[#F9D1DE]"
           className="!text-4xl"
         /> */}
-        <h2 className="max-w-3xl pb-8 mx-auto text-4xl text-center capitalize commonTitle">
+        <h2 className="max-w-3xl pb-8 mx-auto text-4xl text-center capitalize commonTitle mt-10">
           The Only Free Text Messaging <br /> Wedding Platform
         </h2>
         <div className="w-48 mx-auto h-[2px] md:h-[4px] mb-16 bg-[#F9D1DE]" />
         <div className="grid max-w-4xl gap-16 mx-auto md:grid-cols-12 lg:gap-20">
           <div className="self-center col-span-6 md:col-span-7">
             <div className="space-y-5">
-              <div className="flex items-center pr-8 space-x-8 lg:pr-10 timeline pl-7 lg:pl-8 py-7 lg:py-8 lg:space-x-10">
+              <div className="flex items-center justify-between pr-8 space-x-8 lg:pr-10 timeline pl-7 lg:pl-8 py-7 lg:py-8 lg:space-x-10">
                 <p className="text-[15px] md:text-[16px] lg:text-[16px] font-semibold capitalize !leading-[167.5%]">
                   Text, Email, and Mail Out Invitations
                 </p>
--- client/src/components/shared/header/NavLinks.js
+++ client/src/components/shared/header/NavLinks.js
@@ -1,37 +1,38 @@
-
-import ActiveLink from "../ActiveLink";
+import ActiveLink from '../ActiveLink';
 
 const NavLinks = ({ className }) => {
   return (
     <div
-      className={`flex md:items-center md:gap-5  ${className ? className : ""}`}
+      className={`flex md:items-center space-x-5 xl:space-x-[44px] ${
+        className ? className : ''
+      }`}
     >
-      <ActiveLink href="/">
-        <img src="/icons/home.svg" alt="" className="w-5 h-5" />
+      <ActiveLink href='/'>
+        <img src='/icons/home.svg' alt='' className='w-5 h-5' />
         <span>Home</span>
       </ActiveLink>
       {/* <ActiveLink href='/wedding-website'>
         <HeartIcon className='w-5 h-5' />
         <span>Wedding Website</span>
       </ActiveLink> */}
-      <ActiveLink href="/invitations">
+      <ActiveLink href='/invitations'>
         <img
-          src="/icons/chat.svg"
-          alt="Text-Email-Mail Invites"
-          className="w-5 h-5"
+          src='/icons/chat.svg'
+          alt='Text-Email-Mail Invites'
+          className='w-5 h-5'
         />
         {/* <span>Text-Email-Mail Invites</span> */}
         <span>Invitations</span>
       </ActiveLink>
-      <ActiveLink href="/gift-and-registry">
-        <img src="/icons/gift.svg" alt="Registry" className="w-5 h-5" />
+      <ActiveLink href='/gift-and-registry'>
+        <img src='/icons/gift.svg' alt='Registry' className='w-5 h-5' />
         <span>Gifts & Registry</span>
       </ActiveLink>
-      <ActiveLink href="/address-and-rsvp">
+      <ActiveLink href='/address-and-rsvp'>
         <img
-          src="/icons/compass.svg"
-          alt="Need Your Address"
-          className="w-5 h-5"
+          src='/icons/compass.svg'
+          alt='Need Your Address'
+          className='w-5 h-5'
         />
         <span>We Need Your Address</span>
       </ActiveLink>
