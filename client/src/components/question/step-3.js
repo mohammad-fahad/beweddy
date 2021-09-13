6846013c35821d1a221babb2c11f973a50ee605d
Sonjoy Barman <60999976+sonjoybarmon@users.noreply.github.com>
2021-09-13 08:50:01 -0400

    done on user created progress form changes and responsive is done (#115)

--- client/src/components/question/step-3.js
+++ client/src/components/question/step-3.js
@@ -1,18 +1,18 @@
-import { CreateWebsiteContainer } from '@components/createWebsite';
-import { Button, CropImage, Heading, Loader } from '@components/index';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useCallback, useEffect, useState } from 'react';
-import { useDropzone } from 'react-dropzone';
-import { useForm } from 'react-hook-form';
-import { useDispatch, useSelector } from 'react-redux';
-import { Image } from 'cloudinary-react';
-import { addWeddingAnnouncement } from '@features/question/questionSlice';
-import { XIcon } from '@heroicons/react/solid';
-import { attemptImageUpload, removeImage } from '@utils/index';
-import { motion } from 'framer-motion';
-import { isEmpty } from 'lodash';
-import axios from 'axios';
+import { CreateWebsiteContainer } from "@components/createWebsite";
+import { Button, CropImage, Heading, Loader } from "@components/index";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useCallback, useEffect, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
+import { Image } from "cloudinary-react";
+import { addWeddingAnnouncement } from "@features/question/questionSlice";
+import { XIcon } from "@heroicons/react/solid";
+import { attemptImageUpload, removeImage } from "@utils/index";
+import { motion } from "framer-motion";
+import { isEmpty } from "lodash";
+import axios from "axios";
 
 const easing = [0.6, -0.05, 0.01, 0.99];
 const fadeInUp = {
@@ -46,7 +46,7 @@ const UploadAnnouncement = () => {
   const [preview, setPreview] = useState();
   const [selectedImageFile, setSelectedImageFile] = useState();
 
-  const { questions } = useSelector(state => state.question);
+  const { questions } = useSelector((state) => state.question);
   const [uploadedFile, setUploadedFile] = useState(
     questions.weddingAnnouncement?.uploadAnnouncement
   );
@@ -60,36 +60,36 @@ const UploadAnnouncement = () => {
     handleSubmit,
     formState: { errors },
   } = useForm({
-    mode: 'all',
+    mode: "all",
     defaultValues: { uploadAnnouncement: uploadedFile },
   });
 
-  watch(['do_this_later']);
-  const doThisLater = getValues('do_this_later');
+  watch(["do_this_later"]);
+  const doThisLater = getValues("do_this_later");
 
   // React hook form register uploadAnnouncement
   useEffect(() => {
-    register('uploadAnnouncement', {
+    register("uploadAnnouncement", {
       required: {
         value: !doThisLater,
-        message: 'Please upload file or check do this later',
+        message: "Please upload file or check do this later",
       },
     });
   }, [register, doThisLater]);
 
   useEffect(() => {
-    setValue('uploadAnnouncement', uploadedFile);
+    setValue("uploadAnnouncement", uploadedFile);
   }, [uploadedFile]);
 
   useEffect(() => {
     if (doThisLater) {
       setUploadedFile({});
-      clearErrors('uploadAnnouncement');
+      clearErrors("uploadAnnouncement");
     }
   }, [doThisLater]);
 
-  const onSubmit = data => {
-    if (!getValues('do_this_later')) {
+  const onSubmit = (data) => {
+    if (!getValues("do_this_later")) {
       dispatch(addWeddingAnnouncement(data));
     } else {
       dispatch(addWeddingAnnouncement({}));
@@ -98,9 +98,9 @@ const UploadAnnouncement = () => {
     // push('/create-website/step-4', null, { shallow: true });
   };
 
-  const onDrop = useCallback(acceptedFiles => {
+  const onDrop = useCallback((acceptedFiles) => {
     const fileDropped = acceptedFiles[0];
-    if (fileDropped['type'].split('/')[0] === 'image') {
+    if (fileDropped["type"].split("/")[0] === "image") {
       setSelectedImageFile(fileDropped);
       return;
     }
@@ -111,7 +111,7 @@ const UploadAnnouncement = () => {
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
-    accept: 'image/*',
+    accept: "image/*",
     multiple: false,
   });
 
@@ -122,12 +122,12 @@ const UploadAnnouncement = () => {
     const URL = `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`;
     try {
       const formData = new FormData();
-      formData.append('file', file);
-      formData.append('upload_preset', 'beweddy_csfhgnsu');
+      formData.append("file", file);
+      formData.append("upload_preset", "beweddy_csfhgnsu");
 
       const config = {
         headers: {
-          'Content-Type': 'multipart/form-data',
+          "Content-Type": "multipart/form-data",
           // Authorization: `Bearer ${user.token}`,
         },
       };
@@ -143,7 +143,7 @@ const UploadAnnouncement = () => {
       // );
       // const data = await attemptImageUpload(formData);
       setLoading(false);
-      setValue('uploadAnnouncement', {
+      setValue("uploadAnnouncement", {
         public_id,
         height,
         width,
@@ -165,7 +165,7 @@ const UploadAnnouncement = () => {
       setLoading(true);
       await removeImage(uploadedFile.public_id);
       setUploadedFile({});
-      setValue('uploadAnnouncement', {});
+      setValue("uploadAnnouncement", {});
       setLoading(false);
     } catch (err) {
       setLoading(false);
@@ -175,8 +175,8 @@ const UploadAnnouncement = () => {
 
   return (
     <CreateWebsiteContainer
-      page='3'
-      seo={{ title: 'Upload Wedding Invitation & Announcement' }}
+      page="3"
+      seo={{ title: "Upload Wedding Invitation & Announcement" }}
     >
       {loading && <Loader />}
       <motion.form
@@ -190,25 +190,25 @@ const UploadAnnouncement = () => {
           animate={{ opacity: 1, scale: 1 }}
         >
           <Heading
-            label='Upload Wedding invite & announcement!'
-            color='bg-primary'
-            className='!text-[36px]'
-            lineStyle={{ marginBottom: '37px' }}
+            label="Upload Wedding invite & announcement!"
+            color="bg-primary"
+            className="!text-[36px] commonTitle"
+            lineStyle={{ marginBottom: "37px" }}
           />
         </motion.div>
         <motion.div
-          className='w-full flex flex-col items-center justify-center gap-5 md:gap-8 mb-8 md:mb-10'
+          className="flex flex-col items-center justify-center w-full gap-5 mb-8 md:gap-8 md:mb-10"
           variants={fadeInUp}
         >
           {!isEmpty(uploadedFile) && (
-            <div className='max-w-[200px] md:max-w-[190px] w-full mx-auto'>
-              <div className='group border-[3px] border-primary rounded-[5px] overflow-hidden relative'>
+            <div className="max-w-[200px] md:max-w-[190px] w-full mx-auto">
+              <div className="group border-[3px] border-primary rounded-[5px] overflow-hidden relative">
                 <button
-                  type='button'
-                  className='z-50 hidden group-hover:inline-block absolute right-1 top-1 p-1 text-red-400 border border-primary bg-white rounded-full'
+                  type="button"
+                  className="absolute z-50 hidden p-1 text-red-400 bg-white border rounded-full group-hover:inline-block right-1 top-1 border-primary"
                   onClick={handleRemoveImage}
                 >
-                  <XIcon className='w-5 h-5' />
+                  <XIcon className="w-5 h-5" />
                 </button>
                 {/* <div className='aspect-w-1 aspect-h-1'> */}
                 <div>
@@ -217,16 +217,16 @@ const UploadAnnouncement = () => {
                     publicId={uploadedFile.public_id}
                     src={!uploadedFile.public_id ? uploadedFile.url : null}
                     // src={preview}
-                    width='200'
-                    crop='scale'
-                    className='object-cover w-full'
+                    width="200"
+                    crop="scale"
+                    className="object-cover w-full"
                   />
                 </div>
               </div>
               {/* <div className='pb-[80%] group border-[3px] border-primary rounded-[5px] overflow-hidden relative'>
                 <button
                   type='button'
-                  className='hidden group-hover:inline-block absolute right-1 top-1 p-1 text-red-400 border border-primary bg-white rounded-full'
+                  className='absolute hidden p-1 text-red-400 bg-white border rounded-full group-hover:inline-block right-1 top-1 border-primary'
                   onClick={handleRemoveImage}
                 >
                   <XIcon className='w-5 h-5' />
@@ -237,56 +237,56 @@ const UploadAnnouncement = () => {
                   src={!uploadedFile.public_id ? uploadedFile.url : null}
                   width='300'
                   crop='scale'
-                  className='absolute inset-0 h-full object-cover w-full'
+                  className='absolute inset-0 object-cover w-full h-full'
                 />
               </div> */}
             </div>
           )}
           <motion.div
-            className='relative focus:outline-none'
+            className="relative focus:outline-none"
             variants={fadeInUp}
             {...getRootProps()}
           >
             <input {...getInputProps()} />
             <label
-              htmlFor='uploadAnnouncement'
-              className='w-56 bg-white cursor-pointer inline-block text-center text-sm md:text-base font-medium md:font-semibold py-2 md:py-3 px-4 placeholder-primary border-[3px] border-primary rounded-[5px]'
+              htmlFor="uploadAnnouncement"
+              className="w-56 bg-white cursor-pointer inline-block text-center text-sm md:text-base font-medium md:font-semibold py-2 md:py-3 px-4 placeholder-primary border-[3px] border-primary rounded-[5px]"
             >
               Upload
             </label>
-            <p className='mt-2 text-red-400 font-light text-sm text-center'>
+            <p className="mt-2 text-sm font-light text-center text-red-400">
               {errors?.uploadAnnouncement?.message}
             </p>
           </motion.div>
           <motion.div
-            className='flex items-center space-x-3 !-mt-5'
+            className="flex items-center space-x-3 !-mt-5"
             variants={fadeInUp}
           >
             <input
-              type='checkbox'
-              id='do_this_later'
+              type="checkbox"
+              id="do_this_later"
               value={true}
-              className='text-secondary-alternative rounded-md border-2 border-primary w-[20px] md:w-[24px] h-[20px] md:h-[24px] focus:ring-2 focus:ring-offset-2 focus:ring-primary'
-              {...register('do_this_later')}
+              className="text-secondary-alternative rounded-md border-2 border-primary w-[20px] md:w-[24px] h-[20px] md:h-[24px] focus:ring-2 focus:ring-offset-2 focus:ring-primary"
+              {...register("do_this_later")}
             />
             <label
-              htmlFor='do_this_later'
-              className='font-inter text-sm md:text-lg font-normal cursor-pointer'
+              htmlFor="do_this_later"
+              className="text-sm font-normal cursor-pointer font-inter md:text-lg subTitle"
             >
               I will do this later
             </label>
           </motion.div>
         </motion.div>
         <motion.div
-          className='my-1 md:my-5 text-center flex items-center gap-5 flex-wrap sm:flex-nowrap'
+          className="flex flex-wrap items-center gap-5 my-1 text-center md:my-5 sm:flex-nowrap"
           variants={fadeInUp}
         >
           <Button
-            label='Back'
-            className='opacity-50 !bg-[#bebebe] !rounded-[10px]'
+            label="Back"
+            className="opacity-50 !bg-[#bebebe] !rounded-[10px]"
             onClick={() => push({ query: { step: 2 } })}
           />
-          <Button label='Next' type='submit' className='!rounded-[10px]' />
+          <Button label="Next" type="submit" className="!rounded-[10px]" />
         </motion.div>
       </motion.form>
       <CropImage
