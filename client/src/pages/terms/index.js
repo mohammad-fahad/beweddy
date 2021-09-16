d471d357a264087f6c1e26264226f9ba16e334e3
Muttakin Hasib <hasibmolla28@gmail.com>
2021-09-16 16:37:20 +0600

    Updated Gift Cards :zap: :construction: :hammer_and_wrench:

--- client/src/pages/terms/index.js
+++ client/src/pages/terms/index.js
@@ -18,14 +18,14 @@ const Terms = () => {
 
       <div className='bg-gradient-to-br from-[#fddce7] to-white border-t-[2px] border-primary border-b-[2px]'>
         <div className='container grid w-full md:grid-cols-12 gap-4 mt-5'>
-          <div className='p-1 text-lg font-semibold md:col-span-10 md:col-start-2 subTitle'>
+          <div className='p-1 text-sm lg:text-base font-normal md:col-span-10 md:col-start-2 subTitle'>
             <Banner />
           </div>
         </div>
       </div>
 
       <div className='container grid w-full md:grid-cols-12 gap-4 mt-5'>
-        <div className='p-1 text-lg font-semibold md:col-span-10 md:col-start-2 subTitle'>
+        <div className='p-1 text-sm lg:text-base font-normal md:col-span-10 md:col-start-2 subTitle'>
           <Paragraph />
         </div>
       </div>
