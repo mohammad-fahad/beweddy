a612402e1c98509af8787cc7f541e5def2ac041a
Asraful Islam <asraful@iMac.local>
2021-09-18 15:14:55 +0600

    Refactor UI :zap: :hammer:

---
+++ client/src/services/Payment/index.js
@@ -0,0 +1,23 @@
+import { successAlert } from '@helpers/index';
+import { API_URL } from '@utils/index';
+import axios from 'axios';
+
+export const attemptPayment = async payload => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+      // Authorization: `Bearer ${token}`,
+    },
+  };
+
+  try {
+    const { data } = await axios.post(
+      `${API_URL}/create-checkout-session`,
+      payload,
+      config
+    );
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+};
