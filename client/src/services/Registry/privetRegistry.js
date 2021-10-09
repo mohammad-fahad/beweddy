ce7799b3744af48f78967a9f74f35d0fabc4f030
Muttakin Hasib <hasibmolla28@gmail.com>
2021-10-09 22:58:45 +0600

    added privet registry API :zap: :construction: :hammer_and_wrench:

--- client/src/services/Registry/privetRegistry.js
+++ client/src/services/Registry/privetRegistry.js
@@ -2,6 +2,28 @@ import { successAlert } from '@helpers/index';
 import { API_URL } from '@utils/index';
 import axios from 'axios';
 
+export const getPrivetRegistries = async ({ queryKey }) => {
+  const [_key, id] = queryKey;
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+      // Authorization: `Bearer ${token}`,
+    },
+  };
+
+  if (id) {
+    try {
+      const { data } = await axios.post(
+        `${API_URL}/privetRegistries/${id}`,
+        config
+      );
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+};
+
 export const createPrivetRegistry = async ({ payload, token }) => {
   // const [_key] = queryKey;
   const config = {
@@ -14,6 +36,27 @@ export const createPrivetRegistry = async ({ payload, token }) => {
   try {
     const { data } = await axios.post(
       `${API_URL}/privetRegistries/create`,
+      payload,
+      config
+    );
+    successAlert(data.message);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const updatePrivetRegistry = async ({ id, token, payload }) => {
+  // const [_key] = queryKey;
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  try {
+    const { data } = await axios.put(
+      `${API_URL}/privetRegistries/${id}`,
       payload,
       config
     );
