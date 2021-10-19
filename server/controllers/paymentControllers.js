a75d7203655e2fd02ae3230106454e5841593a3e
Muttakin Hasib <hasibmolla28@gmail.com>
2021-10-19 20:01:32 +0600

    added button animation & navigator pushName :zap:

--- server/controllers/paymentControllers.js
+++ server/controllers/paymentControllers.js
@@ -3,6 +3,7 @@ import { nanoid } from 'nanoid';
 import Stripe from 'stripe';
 import { generateTangoToken } from '../utils/token/index.js';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
 // Get All Registry
 export const checkoutSession = asyncHandler(async (req, res) => {
   const token = generateTangoToken({ ...req.body.gift, id: nanoid() });
@@ -28,6 +29,38 @@ export const checkoutSession = asyncHandler(async (req, res) => {
     line_items: [transformedItem],
     mode: 'payment',
     success_url: `${process.env.CLIENT_URL}/payment/${token}`,
+    // cancel_url: `${process.env.CLIENT_URL}/couple/${req.body.cancel}`,
+    cancel_url: `${process.env.CLIENT_URL}`,
+    // metadata: {
+    //   email,
+    //   images: JSON.stringify(items.map(item => item.image)),
+    // },
+  });
+  res.json(session.url);
+});
+
+export const mailoutPayment = asyncHandler(async (req, res) => {
+  const transformedItem = {
+    description: req.body.description,
+    quantity: 1,
+    price_data: {
+      currency: 'usd',
+      unit_amount: Number(req.body.price) * 100,
+      product_data: {
+        name: req.body.title,
+        images: [req.body.image],
+      },
+    },
+  };
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    shipping_address_collection: {
+      allowed_countries: ['US'],
+    },
+    line_items: [transformedItem],
+    mode: 'payment',
+    success_url: `${process.env.CLIENT_URL}/mailout?payment=success`,
     // cancel_url: `${process.env.CLIENT_URL}/couple/${req.body.cancel}`,
     cancel_url: `${process.env.CLIENT_URL}`,
     // metadata: {
