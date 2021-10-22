ec12f4b590f96a2329c623fbdd464bd21df9fdbc
sonjoybarmon <sonjoybarmon19@gmail.com>
2021-10-22 22:01:22 +0600

    modidy mailouts

--- client/src/components/MailOuts/FullScreenImage.jsx
+++ client/src/components/MailOuts/FullScreenImage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import fileDownload from "js-file-download";
 import Draggable from "react-draggable";
@@ -21,15 +21,14 @@ export default function FullScreenImage({
     fileDownload(image, "beweddy.png");
     setShowModal(false);
   };
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    setProduct(JSON.parse(localStorage.getItem("mailout")) || []);
+  }, []);
+
   return (
     <>
-      {/* <button
-        className="px-6 py-3 mb-1 mr-1 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear bg-pink-500 rounded shadow outline-none active:bg-pink-600 hover:shadow-lg focus:outline-none"
-        type="button"
-        onClick={() => setShowModal(true)}
-      >
-        Open large modal
-      </button> */}
       <button
         onClick={() => setShowModal(true)}
         className={`!w-[95px] !h-[36px] bg-[#FCE0EB] font-semibold transition duration-300 font-inter text-[12px]`}
@@ -44,7 +43,9 @@ export default function FullScreenImage({
               <div className="relative flex flex-col w-full bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none">
                 {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid rounded-t border-blueGray-200">
-                  <h3 className="text-3xl font-semibold">Image Full Screen</h3>
+                  <h3 className="text-3xl font-semibold subTitle">
+                    Image Full Screen
+                  </h3>
                   <button
                     className="float-right p-1 ml-auto text-3xl font-semibold leading-none text-black bg-transparent border-0 outline-none opacity-5 focus:outline-none"
                     onClick={() => setShowModal(false)}
@@ -55,7 +56,129 @@ export default function FullScreenImage({
                   </button>
                 </div>
                 {/*body*/}
-                <div className="relative flex-auto p-2">
+                <div class="grid grid-cols-12 w-full">
+                  <div class="sm:col-span-6 col-span-12 ">
+                    <div className="relative flex-auto p-2">
+                      {/* <Image
+                        src={product[0]?.selected}
+                        width={1200}
+                        height={1500}
+                        // layout="responsive"
+                        className="rounded-lg w-[80%] mx-auto modalImage"
+                        objectFit="contain"
+                      /> */}
+                      <img
+                        src={product[0]?.selected}
+                        className="rounded-lg w-[100%] mx-auto modalImage"
+                        loading="lazy"
+                      />
+                      <div className="absolute top-0 bottom-0 left-0 right-0 text-center">
+                        <Draggable>
+                          <div className="flex flex-col items-center justify-center w-full h-[100%]">
+                            <h2
+                              style={{
+                                color: `${textColor.color}`,
+                                fontFamily: `${textFont.font}`,
+                              }}
+                              className={`text-[30px] font-medium leading-10 capitalize subTitle`}
+                            >
+                              {user?.fullName}
+                            </h2>
+                            <h4
+                              style={{
+                                color: `${textColor.color}`,
+                                fontFamily: `${textFont.font}`,
+                              }}
+                              className={`text-[30px] font-medium leading-10 capitalize subTitle`}
+                            >
+                              And
+                            </h4>
+                            <h2
+                              className={`text-[30px] font-medium leading-10 capitalize subTitle`}
+                              style={{
+                                color: `${textColor.color}`,
+                                fontFamily: `${textFont.font}`,
+                              }}
+                            >
+                              <span>{user?.questions.spouseFirstName}</span>
+                              <span className="ml-2">
+                                {user?.questions.spouseLastName}
+                              </span>
+                            </h2>
+                            <h4
+                              style={{
+                                color: `${textColor.color}`,
+                                fontFamily: `${textFont.font}`,
+                              }}
+                              className={`text-[18px] font-medium leading-10 capitalize customLabel sm:max-w-full max-w-[150px] `}
+                            >
+                              We’re getting married
+                              <span className="ml-1">
+                                {isoToUtcDate(
+                                  user?.questions?.weddingDay?.weddingDate
+                                )}{" "}
+                              </span>
+                            </h4>
+                          </div>
+                        </Draggable>
+                      </div>
+                    </div>
+                  </div>
+                  <div class=" sm:col-span-6 col-span-12">
+                    <div className="relative flex-auto p-2 ">
+                      {/* <Image
+                        src={product[0]?.backPart}
+                        width={1200}
+                        height={1500}
+                        // layout="responsive"
+                        className="rounded-lg w-[80%] mx-auto modalImage"
+                        objectFit="contain"
+                      /> */}
+
+                      <img
+                        src={product[0]?.backPart}
+                        className="rounded-lg w-[100%] mx-auto modalImage"
+                        loading="lazy"
+                      />
+                      <div className="absolute top-0 bottom-0 left-0 right-0 text-center">
+                        <div className="flex flex-col items-center justify-center w-full h-[95%]">
+                          <div className="mb-[30px]">
+                            <img
+                              src={uploadedFile?.url}
+                              // src={user?.questions?.couplePictures[0]?.url}
+                              // alt=""
+                              className="!md:max-h-[150px] !h-[150px] max-w-[300px] mx-auto mailoutImage2"
+                            />
+                          </div>
+                          <div>
+                            <h1 className="max-w-[230px] text-[14px] mb-[20px] customLabel">
+                              Scan Our Personalized QR Code To Visit Our Wedding
+                              Website
+                            </h1>
+                          </div>
+                          <div className="flex items-center justify-center qrCode">
+                            <QRCode
+                              {...{ value }}
+                              size={75}
+                              eyeRadius={[
+                                {
+                                  outer: [10, 10, 0, 10],
+                                  inner: [0, 10, 10, 10],
+                                },
+                                [10, 10, 10, 0], // top/right eye
+                                [10, 0, 10, 10], // bottom/left
+                              ]}
+                              logoHeight={20}
+                              logoWidth={20}
+                              logoImage="/icons/circle-ring.png"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* <div className="relative flex-auto p-2">
                   <Image
                     src={image}
                     width={1200}
@@ -150,7 +273,7 @@ export default function FullScreenImage({
                       </div>
                     )}
                   </div>
-                </div>
+                </div> */}
                 {/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid rounded-b border-blueGray-200">
                   <button
