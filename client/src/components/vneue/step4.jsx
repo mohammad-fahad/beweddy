a788ad875b89908e51cb540d5ea260c81f2c583a
sonjoybarmon <sonjoybarmon19@gmail.com>
2021-11-06 20:59:22 +0600

    responsive is done

--- client/src/components/vneue/step4.jsx
+++ client/src/components/vneue/step4.jsx
@@ -1,9 +1,9 @@
-import { Button, Heading, CreateWebsiteContainer } from '@components/index';
-import { addplan } from '@features/question/venueSlice';
-import { motion } from 'framer-motion';
-import { useRouter } from 'next/router';
-import { useForm } from 'react-hook-form';
-import { useDispatch } from 'react-redux';
+import { Button, Heading, CreateWebsiteContainer } from "@components/index";
+import { addplan } from "@features/question/venueSlice";
+import { motion } from "framer-motion";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
 
 const Package = () => {
   const { push } = useRouter();
@@ -13,62 +13,62 @@ const Package = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({ mode: 'all' });
+  } = useForm({ mode: "all" });
 
-  const onSubmit = data => {
+  const onSubmit = (data) => {
     // console.log("bisnessName", data);
     // dispatch(addBusinessLink(data));
-    dispatch(addplan('none'));
+    dispatch(addplan("none"));
     push({ query: { step: 5 } });
   };
 
   return (
-    <CreateWebsiteContainer seo={{ title: 'Couple Names' }} page='1'>
+    <CreateWebsiteContainer seo={{ title: "Couple Names" }} page="4">
       <form
         className={`flex flex-col items-center justify-center w-full -mt-9 sm:mt-0`}
         onSubmit={handleSubmit(onSubmit)}
       >
-        <h2 className='pb-8 mx-auto text-[36px] text-center commonTitle'>
+        <h2 className="pb-8 mx-auto text-[36px] text-center commonTitle">
           Plan & Pricing
         </h2>
-        <div className='w-48 mx-auto h-[2px] md:h-[4px] mb-16 bg-primary' />
+        <div className="w-48 mx-auto h-[2px] md:h-[4px] mb-16 bg-primary" />
 
         {/* </motion.div> */}
         <motion.div
-          className='flex flex-col flex-wrap items-center justify-center gap-2 md:gap-5 xl:flex-row'
+          className="flex flex-col flex-wrap items-center justify-center gap-2 md:gap-5 xl:flex-row"
           exit={{ opacity: 0 }}
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
         >
-          <div class='grid grid-cols-12 w-full border-2 border-current rounded max-w-[1027px]  p-9 '>
-            <div class='sm:col-span-6 col-span-12 p-2'>
-              <h1 className='font-inter font-semibold text-[32px] leading-[44px]'>
+          <div class="grid grid-cols-12 w-full border-2 border-current rounded max-w-[1027px]  p-9 ">
+            <div class="sm:col-span-6 col-span-12 p-2">
+              <h1 className="font-inter font-semibold text-[32px] leading-[44px] subTitle">
                 Premium Subscription Plan
               </h1>
-              <h3 className='font-inter font-normal text-[24px] leading-[34px] mt-3'>
+              <h3 className="font-inter font-normal text-[24px] leading-[34px] mt-3 customLabel">
                 For venue that need advanced features, services and support
               </h3>
             </div>
-            <div class='sm:col-span-6 col-span-12 p-2 flex flex-col items-center justify-center'>
-              <h1 className='font-inter font-semibold text-[32px] leading-[44px]'>
+            <div class="sm:col-span-6 col-span-12 p-2 flex flex-col items-center justify-center">
+              <h1 className="font-inter font-semibold text-[32px] leading-[44px] subTitle">
                 $50/m
               </h1>
-              <button className='border-2 border-current rounded px-5 py-3 text-[16px] font-bold mt-3'>
+              <button className="border-2 border-current rounded px-5 py-3 text-[16px] font-bold mt-3 customLabel">
                 Free 30 Day Trial
               </button>
             </div>
           </div>
         </motion.div>
-        <div className='flex flex-wrap items-center gap-5 my-3 text-center md:my-10 sm:flex-nowrap'>
+        <div className="flex flex-wrap items-center gap-5 my-3 text-center md:my-10 sm:flex-nowrap">
           <Button
-            label='Back'
-            className='opacity-50 !bg-[#bebebe] !rounded-[10px] !w-[178px] !h-[59px]'
+            label="Back"
+            className="opacity-50 !bg-[#bebebe] !rounded-[10px] !w-[178px] !h-[59px]"
             onClick={() => push({ query: { step: 3 } })}
           />
           <Button
-            label='Next'
-            type='submit'
-            className='!rounded-[10px] !w-[178px] !h-[59px]'
+            label="Next"
+            type="submit"
+            className="!rounded-[10px] !w-[178px] !h-[59px]"
           />
         </div>
       </form>
