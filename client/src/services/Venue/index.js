addf7f68aceace2006d2cc99a636496845770826
Hasib Molla <hasibmolla28@gmail.com>
2022-01-09 01:01:45 +0600

    :hammer:

--- client/src/services/Venue/index.js
+++ client/src/services/Venue/index.js
@@ -1,11 +1,11 @@
 // import { successAlert } from '@helpers/index';
-import { API_URL } from '@utils/index';
-import axios from 'axios';
+import { API_URL } from "@utils/index";
+import axios from "axios";
 
 export const getVenues = async () => {
   const config = {
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       // Authorization: `Bearer ${token}`,
     },
   };
@@ -15,5 +15,25 @@ export const getVenues = async () => {
     return data;
   } catch (err) {
     console.error(err);
+  }
+};
+
+export const getVenue = async ({ queryKey }) => {
+  const [_key, id] = queryKey;
+
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      // Authorization: `Bearer ${token}`,
+    },
+  };
+
+  if (id) {
+    try {
+      const { data } = await axios.get(`${API_URL}/venues/${id}`, config);
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
   }
 };
--- server/routes/venueRoutes.js
+++ server/routes/venueRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getVenues } from '../controllers/venueControllers.js';
+import { getVenue, getVenues } from '../controllers/venueControllers.js';
 import { protect } from '../middlewares/auth.js';
 
 const router = express.Router();
 
 router.get('/', getVenues);
+router.get('/:id', getVenue);
 
 export default router;
