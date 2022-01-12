6803012fa9ffca16fe6c99e325be8a64f9c92751
Hasib Molla <hasibmolla28@gmail.com>
2022-01-13 01:26:39 +0600

    :hammer::zap:

--- client/src/pages/couple/[couple]/index.js
+++ client/src/pages/couple/[couple]/index.js
@@ -280,7 +280,7 @@ const CoupleWebsitePage = (props) => {
                 </div>
               )}
               {couple?.location && (
-                <div class="sm:col-start-2 col-span-4 pl-3 flex justify-center">
+                <div class="col-start-2 col-span-4 pl-3 flex justify-center">
                   <div>
                     <h2 className="text-lg">Locations</h2>
                     <h6>{couple?.location}</h6>
